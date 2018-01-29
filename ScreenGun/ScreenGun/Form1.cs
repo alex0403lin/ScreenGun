7e844f6b0308cdd977299726dc61dd37da6af795
LINYUNBAO <LINYUNBAO@DESKTOP-6NPT1NK>
2018-01-29 21:00:39 +0800

    Add ScreenGun project.

---
+++ ScreenGun/ScreenGun/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ScreenGun
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+        public Form2 F2 = null;
+        public static int SetValueX;
+        public static int SetValueY;
+        public static int SetValueWidth;
+        public static int SetValueHeight;
+
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        public void SetValue(int LeftX, int TopY,int RightX,int BottomY) {
+            SetValueX = LeftX;
+            SetValueY = TopY;
+            SetValueHeight = BottomY - TopY;
+            SetValueWidth = RightX - LeftX;
+
+            label1.Text = "(" + LeftX.ToString() + "," + TopY.ToString() + ") "+
+                "(" + RightX.ToString() + "," + BottomY.ToString() + ")";
+            label2.Text = SetValueHeight.ToString()+","+SetValueWidth.ToString();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(F2 ==null)
+            {
+                F2 = new Form2(this);
+                F2.Owner = this;
+            }
+            F2.Show();
+            this.Hide();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Bitmap bmp = new Bitmap(SetValueWidth, SetValueHeight);
+            Graphics graphics = Graphics.FromImage(bmp);
+            graphics.CompositingQuality = System.Drawing.Drawing2D.CompositingQuality.HighQuality;
+            graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+            graphics.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+            graphics.CopyFromScreen(new Point(SetValueX,SetValueY),new Point(0, 0),new Size(SetValueWidth, SetValueHeight));
+            IntPtr intPtr = graphics.GetHdc();
+            graphics.ReleaseHdc(intPtr);
+            
+            bmp.SetResolution(SetValueWidth, SetValueHeight);
+            this.pictureBox1.Image = bmp;
+
+            if (bmp != null)
+            {
+                bmp.Save("c:\\myBitmap.jpg");
+               
+            }
+            
+        }
+
+    }
+}
---
+++ ScreenGun/ScreenGun/Form2.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ScreenGun
+{
+    public partial class Form2 : Form
+    {
+        public Form1 F1;
+        private Rectangle rect;
+        public static int PositionLeftX;
+        public static int PositionTopY;
+        public static int PositionRightX;
+        public static int PositionBottomY;
+
+
+        public Form2(Form1 owner1)
+        {
+            InitializeComponent();
+            this.WindowState = FormWindowState.Maximized;
+            F1 = owner1;
+        }
+
+
+
+        private void Form2_MouseDown(object sender, MouseEventArgs e)
+        {
+            this.Cursor = new Cursor(Cursor.Current.Handle);
+            Cursor.Position = new Point(Cursor.Position.X, Cursor.Position.Y);
+            label1.Text = Cursor.Position.ToString();
+
+            PositionLeftX = Cursor.Position.X;
+            PositionTopY = Cursor.Position.Y;
+            // "e.X" and "e.Y"為滑鼠座標
+            rect = new Rectangle(e.X, e.Y, 0, 0);
+            this.Invalidate();
+        }
+
+        private void Form2_MouseUp(object sender, MouseEventArgs e)
+        {
+            this.Cursor = new Cursor(Cursor.Current.Handle);
+            Cursor.Position = new Point(Cursor.Position.X, Cursor.Position.Y);
+            PositionRightX = Cursor.Position.X;
+            PositionBottomY = Cursor.Position.Y;
+            label1.Text += Cursor.Position.ToString();
+
+
+
+        }
+
+        private void Form2_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                //滑鼠邊移動邊畫方框
+              
+                rect= new Rectangle(rect.Left, rect.Top, e.X - rect.Left, e.Y - rect.Top);
+            }
+            this.Invalidate();
+        }
+
+        private void Form2_Paint(object sender, PaintEventArgs e)
+        {
+            //Green: 綠色, 可更改為其他顏色; 1: 線寬, 可更改.
+            using (Pen pen = new Pen(Color.Blue, 1))
+            {
+                e.Graphics.DrawRectangle(pen, rect);
+            }
+ 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            F1.SetValue(PositionLeftX, PositionTopY, PositionRightX, PositionBottomY);
+            F1.Show();
+            F1.Focus();
+            this.Hide();
+        }
+    }
+}
